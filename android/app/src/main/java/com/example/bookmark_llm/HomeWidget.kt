10d669e51fe59aa0dfbb5dfaabd68dda25be6829
ekakshjanweja <jekaksh@gmail.com>
2024-06-16 16:20:33 +0530

    temp

---
+++ android/app/src/main/java/com/example/bookmark_llm/HomeWidget.kt
@@ -0,0 +1,53 @@
+package com.example.bookmark_llm
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import  es.antonborri.home_widget.HomeWidgetPlugin
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class HomeWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            val widgetData = HomeWidgetPlugin.getData(context)
+
+            val views = RemoteViews(context.packageName, R.layout.home_widget).apply {
+                val title = widgetData.getString("headline_title", null)
+                setTextViewText(R.id.headline_title, title ?: "No Title")
+            }
+
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val widgetText = context.getString(R.string.appwidget_text)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.home_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
